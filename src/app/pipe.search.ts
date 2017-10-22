d5a663971bc02f0bffae4fbfe6a6340560a3d39c
Ola <arvin@oleeeunited.com>
2017-10-22 13:10:56 +0300

    test

--- src/app/pipe.search.ts
+++ src/app/pipe.search.ts
@@ -8,6 +8,7 @@ export class SearchPipe implements PipeTransform{
     transform(pipeData, pipeModifier) {
         return pipeData.filter((eachItem)=> {
              return eachItem.toLowerCase().includes(pipeModifier.toLowerCase());
+             var test;
         });
     }
 }
